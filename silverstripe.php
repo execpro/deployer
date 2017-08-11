b2b540015f609be9579612316599424fd4c6e31e
Klem Novak (knovak@execproinc.com) <knovak@execproinc.com>
2017-08-11 13:53:00 -0700

    Silverstripe & sync tasks updated

---
+++ silverstripe.php
@@ -0,0 +1,108 @@
+<?php
+
+
+namespace Deployer;
+
+
+
+/**
+ * Silverstripe tasks
+ */
+
+task('silverstripe:build', function () {
+    return run('sudo su - apache -c {{bin/php}} {{deploy_path}}/current/framework/cli-script.php /dev/build"');
+})->desc('Run /dev/build');
+
+task('silverstripe:buildflush', function () {
+    return run('sudo su - apache -c "{{bin/php}} {{deploy_path}}/current/framework/cli-script.php /dev/build flush=all"');
+})->desc('Run /dev/build?flush=all');
+
+task('apache:restart', function () {
+    run('sudo service httpd start');
+});
+
+
+//Create and _ss_environment.php values from the settings
+task('silverstripe:env', function () {
+    if(fileExists(parse('{{deploy_path}}') . '/releases/_ss_environment.php')) {
+        writeln('releases/_ss_environment.php present');
+        return false;
+    }
+    if (askConfirmation('Do you want to generate and upload the _ss_environment.php file?')) {
+        $basepath = dirname(__FILE__) . '/vendor/eis/deployer/templates';
+
+        //Import secrets
+        $secrets = get('silverstripe');
+
+        //Prepare replacement variables
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveArrayIterator($secrets)
+        );
+
+        $replacements = [];
+
+        foreach ($iterator as $key => $value) {
+            $keys = [];
+            for ($i = $iterator->getDepth(); $i > 0; $i --) {
+                $keys[] = $iterator->getSubIterator($i - 1)->key();
+            }
+            $keys[] = $key;
+
+            $replacements['{{' . implode('.', $keys) . '}}'] = $value;
+        }
+
+        $credentials = $secrets;
+
+        //Create settings from template
+        $settings = file_get_contents($basepath . '/_ss_environment.php.tpl');
+
+
+        // DB Name
+        if($secrets["db_name"]) {
+            $settings = strtr($settings, array(
+                "//define('SS_DATABASE_NAME', 'db');" => "define('SS_DATABASE_NAME', '" . $secrets["db_name"] . "');"
+            ));
+        }
+
+        // DB Prefix
+        if($secrets["db_prefix"]) {
+            $settings = strtr($settings, array(
+                "//define('SS_DATABASE_PREFIX', 'db_prefix');" => "define('SS_DATABASE_PREFIX', '" . $secrets["db_prefix"] . "');"
+            ));
+        }
+
+        // DB Suffix
+        if($secrets["db_suffix"]) {
+            $settings = strtr($settings, array(
+                "//define('SS_DATABASE_SUFFIX', 'db_suffix');" => "define('SS_DATABASE_SUFFIX', '" . $secrets["db_suffix"] . "');"
+            ));
+        }
+
+
+        // Enable $file_to_url_mapping
+        if(!$secrets["disable_file_to_url_mapping"]) {
+            $settings = strtr($settings, array(
+                "//\$_FILE_TO_URL_MAPPING['/path/to/release']" => "\$_FILE_TO_URL_MAPPING[realpath('" . parse('{{deploy_path}}') ."/current')]",
+                "//global \$_FILE_TO_URL_MAPPING;" => "global \$_FILE_TO_URL_MAPPING;"
+            ));
+        }
+
+        $settings = strtr($settings, $replacements);
+
+        // Enable admin settings
+        if($secrets["admin_enable"]) {
+            $settings = strtr($settings, array(
+                "//define('SS_DEFAULT_ADMIN_USERNAME" => "define('SS_DEFAULT_ADMIN_USERNAME",
+                "//define('SS_DEFAULT_ADMIN_PASSWORD" => "define('SS_DEFAULT_ADMIN_PASSWORD"
+            ));
+        }
+
+        $tmpFilename = tempnam($basepath, '_ss_environment_tmp');
+        file_put_contents($tmpFilename, $settings);
+
+        writeln('_ss_environment.php created succesfuly');
+
+        upload($tmpFilename, '{{deploy_path}}/releases/_ss_environment.php');
+        unlink($tmpFilename);
+    }
+});
--- sync_tasks.php
+++ sync_tasks.php
@@ -2,94 +2,124 @@
 
 namespace Deployer;
 
+
+
 /**
  * Sync from remote server to local
  */
 
-task(
-	'sync:db',
-	function() {
-		$filename = 'db/' . date('Y-m-d-H-s') . '.sql';
-		$credentials = \WebTorque\Deployment\Environment::inst(getRemoteEnvironment())->parseEnvironment();
 
-		if (!directoryExists('db')) {
-			run("mkdir db");
+task(
+     'assets:pull',
+     function() {
+         askConfirmation('This will compress and download the remote assets folder, continue?');
+
+         $remoteAssets = parse('{{deploy_path}}/shared');
+         //  $filename = 'assets-' . date('Y-m-d-H-s') . '.zip';
+         $filename = 'assets.zip';
+         $tmpFile = '/tmp/' . $filename;
+
+         writeln('<info>Zipping up remote assets</info>');
+         run("cd {$remoteAssets} && zip -r {$tmpFile} assets -x **/*/_resampled/*");
+
+         writeln('<info>Downloading assets file</info>');
+         download($filename, $tmpFile);
+
+         if(askConfirmation('Do you want to delete the remote ZIP file?', false)) {
+             run("rm {$tmpFile}");
+             writeln('<info>Remote assets file deleted.</info>');
+         }
+
+     }
+)
+->desc('Creates a ZIP of remote assets (all files) and downloads it.');
+
+
+task(
+     'assets:pull_images',
+     function() {
+
+         askConfirmation('This will compress and download the remote assets folder, continue?');
+
+         $remoteAssets = parse('{{deploy_path}}/shared');
+        //  $filename = 'assets-' . date('Y-m-d-H-s') . '.zip';
+         $filename = 'assets.zip';
+         $tmpFile = '/tmp/' . $filename;
+
+         writeln('<info>Zipping up remote assets</info>');
+
+         run("cd {$remoteAssets} && zip -r {$tmpFile} assets -x **/*/_resampled/* -x *.mov -x *.wmv -x *.wav -x *.mp4 -x *.mp3 -x *.mkv");
+
+         writeln('<info>Downloading assets file</info>');
+
+         download($filename, $tmpFile);
+
+         if(askConfirmation('Do you want to delete the remote ZIP file?', false)) {
+             run("rm {$tmpFile}");
+             writeln('<info>Remote assets file deleted.</info>');
+         }
+
+
+     }
+)
+->desc('Creates a ZIP of remote assets (images only) and downloads it.');
+
+
+task(
+	'db:pull',
+	function() {
+        $credentials = get('silverstripe');
+
+        if (!directoryExists('{{deploy_path}}/shared/db')) {
+            writeln('<info>Creating shared/db folder</info>');
+			run("mkdir {{deploy_path}}/shared/db");
 		}
 
 		writeln('<info>Dumping remote database</info>');
-		$remoteFilename = getWorkingPath() . DIRECTORY_SEPARATOR . $filename;
-		run("mysqldump -u {$credentials['username']} -p{$credentials['password']} -B {$credentials['database']} > {$remoteFilename}");
 
-		writeln('<info>Downloading database file</info>');
+        $filename = "db-" . date('Y-m-d-H-s') . '.sql';
+        $remoteFilename = parse('{{deploy_path}}/shared/db/') . $filename;
+
+        $host = '';
+        if(array_key_exists("db_host", $credentials) && $credentials["db_host"]) {
+            $host = '-h ' .  $credentials["db_host"];
+        }
+		run("mysqldump --user='{$credentials['db_user']}' --password='{$credentials['db_password']}' {$host} {$credentials['db_name']} > {$remoteFilename}", true);
+
+        writeln('<info>Downloading db file</info>');
 		download($filename, $remoteFilename);
 
-		run("rm {$remoteFilename}");
+        if(askConfirmation('Do you want to delete the remote database file?', false)) {
+		    run("rm {$remoteFilename}");
+        }
 
 		//store so db:import can pick it up
 		set('db_file', $filename);
 	}
 )
-	->desc('Copies database between two servers');
+	->desc('Downloads database between two servers');
 
 
 task(
-	'sync:assets_remote',
+	'db:local_import',
 	function() {
-		askConfirmation('This will compress the assets folder on the remote server, make sure there is enough disk space before continuing');
 
-		$remoteAssets = parse('{{deploy_path}}/shared/' . get('web_dir'));
-        $localAssets = get('web_dir');
-		$tmpFile = '/tmp/assets.tgz';
+        if($dbFile = get('db_file')) {
 
-        writeln('<info>Compressing assets</info>');
-		runLocally("cd {$localAssets} && tar -czf {$tmpFile} assets");
+            $localDBUser = ask("Enter local DB username (MAMP default = root)", "root");
+            $localDBPass = ask("Enter local DB pass (MAMP default = root)", "root");
+            $localDBName = ask("Enter local DB name");
 
-        writeln('<info>Uploading compressed assets</info>');
-		upload($tmpFile, $tmpFile);
-		runLocally("rm {$tmpFile}");
+            runLocally("mysql --user='{$localDBUser}' --password='{$localDBPass}' {$localDBName} < {$dbFile}");
 
-        writeln('<info>Setting up remote assets</info>');
-		run("rm -Rf {$remoteAssets}/assets");
-		run("cd /tmp && tar -xzf {$tmpFile}");
-		run("mv /tmp/assets {$remoteAssets}/assets");
-		run("rm {$tmpFile}");
+        }
+        else {
+            writeln('<error>Local DB file not found</error>');
+        }
+
 	}
-)->desc('Synching remote assets directory');
-
-task(
-    'sync:assets',
-    function() {
-        askConfirmation('This will upload the compreseed assets folder to the remote server, make sure there is enough disk space before continuing');
-
-        $remoteAssets = parse('{{deploy_path}}/shared/' . get('web_dir'));
-        $tmpFile = '/tmp/assets.tgz';
-
-        writeln('<info>Compressing assets</info>');
-        run("cd {$remoteAssets} && tar -czf {$tmpFile} assets");
-
-        writeln('<info>Downloading compressed assets</info>');
-        download($tmpFile, $tmpFile);
-        run("rm {$tmpFile}");
-
-        writeln('<info>Setting up local assets</info>');
-        runLocally('rm -Rf public_html/assets');
-        runLocally("cd /tmp && tar -xzf {$tmpFile}");
-        runLocally('mv /tmp/assets public_html/assets');
-        runLocally("rm {$tmpFile}");
-    }
-)->desc('Synching assets directory');
-
-
-task(
-    'sync:uploaddb',
-    function(){
-        $file = get('db_file');
-
-        if (!$file) $file = ask('Enter the path to the file to import', '');
-
-        upload($file, $file);
-    }
-)->desc('Uploading database to remote server');
+)
+	->desc('Import dowmnloaded remote db file into MAMP');
 
 task(
 	'sync:cleanup',
@@ -103,26 +133,108 @@ task(
 
 
 task(
-	'sync',
+	'db:remote_to_local',
 	[
-		'sync:db',
-		'db:import',
-		'sync:assets',
+		'db:pull',
+		'db:local_import',
 		'sync:cleanup'
 	]
-)->desc('Download and setup database and assets');
+)->desc('Download and import database and assets');
 
 
-/**
- * Sync from local to remote server
- */
-task(
-    'sync:remote',
-    [
-        'db:export',
-        'sync:uploaddb',
-        'db:import_remote',
-        'sync:assets_remote',
-        'sync:cleanup'
-    ]
-)->desc('Upload local db/assets to remote site');
+
+
+
+
+
+
+
+// task(
+// 	'sync:assets_download',
+// 	function() {
+// 		askConfirmation('This will compress the assets folder on the remote server, make sure there is enough disk space before continuing');
+//
+// 		$remoteAssets = parse('{{deploy_path}}/shared/' . get('web_dir'));
+//         $localAssets = get('web_dir');
+// 		$tmpFile = '/tmp/assets.tgz';
+//
+//         writeln('<info>Compressing assets</info>');
+// 		runLocally("cd {$localAssets} && tar -czf {$tmpFile} assets");
+//
+//         writeln('<info>Uploading compressed assets</info>');
+// 		upload($tmpFile, $tmpFile);
+// 		runLocally("rm {$tmpFile}");
+//
+//         writeln('<info>Setting up remote assets</info>');
+// 		run("rm -Rf {$remoteAssets}/assets");
+// 		run("cd /tmp && tar -xzf {$tmpFile}");
+// 		run("mv /tmp/assets {$remoteAssets}/assets");
+// 		run("rm {$tmpFile}");
+// 	}
+// )->desc('Synching remote assets directory');
+//
+// task(
+//     'sync:assets',
+//     function() {
+//         askConfirmation('This will upload the compreseed assets folder to the remote server, make sure there is enough disk space before continuing');
+//
+//         $remoteAssets = parse('{{deploy_path}}/shared/' . get('web_dir'));
+//         $tmpFile = '/tmp/assets.tgz';
+//
+//         writeln('<info>Compressing assets</info>');
+//         run("cd {$remoteAssets} && tar -czf {$tmpFile} assets");
+//
+//         writeln('<info>Downloading compressed assets</info>');
+//         download($tmpFile, $tmpFile);
+//         run("rm {$tmpFile}");
+//
+//         writeln('<info>Setting up local assets</info>');
+//         runLocally('rm -Rf public_html/assets');
+//         runLocally("cd /tmp && tar -xzf {$tmpFile}");
+//         runLocally('mv /tmp/assets public_html/assets');
+//         runLocally("rm {$tmpFile}");
+//     }
+// )->desc('Synching assets directory');
+
+
+// Download remote db and Update local
+
+
+
+
+// task(
+//     'sync:uploaddb',
+//     function(){
+//         $file = get('db_file');
+//
+//         if (!$file) $file = ask('Enter the path to the file to import', '');
+//
+//         upload($file, $file);
+//     }
+// )->desc('Uploading database to remote server');
+
+
+// task(
+// 	'sync',
+// 	[
+// 		'sync:db_pull',
+// 		'db:import',
+// 		'sync:assets_get_remote',
+// 		'sync:cleanup'
+// 	]
+// )->desc('Download and setup database and assets');
+//
+//
+// /**
+//  * Sync from local to remote server
+//  */
+// task(
+//     'sync:local_to_remote',
+//     [
+//         'db:export',
+//         'sync:uploaddb',
+//         'db:import_remote',
+//         'sync:assets_remote',
+//         'sync:cleanup'
+//     ]
+// )->desc('Upload local db/assets to remote site');
